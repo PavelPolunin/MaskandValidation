eb82da0492592c952c481fc1683b3e1cb42fbc6b
Pavel <Anya@LAPTOP-K4AVO9IM>
2024-10-20 15:23:55 +0300

    Добавьте файлы проекта.

---
+++ MaskandValidation/FormAddUsers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaskandValidation.DBContext;
+
+namespace MaskandValidation
+{
+    public partial class FormAddUsers : Form
+    {
+        public FormAddUsers()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormAddUsers_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
